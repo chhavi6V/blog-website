2a5dce1ceddd5c97ae60b78383ebf45010c75359
chhavi6V <chhavigupta597@gmail.com>
2023-06-08 00:34:57 +0530

    final commit

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash");
 let posts = [];
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -16,7 +17,10 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", function(req,res){
-  res.render("home" ,{homeStartingContent : homeStartingContent});
+  res.render("home" ,{
+    homeStartingContent : homeStartingContent ,
+     posts : posts
+    });
 })
 
 app.get("/contact", function(req,res){
@@ -37,8 +41,21 @@ app.post("/compose",function(req,res){
     postBody : req.body.postBody,
   };
   posts.push(post);
-  console.log(posts);
   res.redirect("/"); 
+})
+
+app.get("/posts/:postName", function(req,res){
+    const reqTitle = _.lowerCase(req.params.postName);
+
+    posts.forEach(function(post){
+      const storedTitle = _.lowerCase(post.postTitle);
+      if(storedTitle === reqTitle){
+        res.render("post",{
+          title : post.postTitle,
+          content : post.postBody 
+        });
+      }
+    }) 
 })
 
 app.listen(3000, function() {
